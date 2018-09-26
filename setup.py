6c2bd59eba2e7e4caa85426e8927780faf3efc04
Edwin Hermans <edwin@madtech.cx>
2018-09-26 14:16:43 -0400

    New release, upgrade to beta

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="dveo",
-    version="0.1.0",
+    version="0.2.0",
     description="DVEO REST API wrapper",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -16,7 +16,7 @@ setup(
     author="Edwin Hermans",
     author_email="edwin@madtech.cx",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
